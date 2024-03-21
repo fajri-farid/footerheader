8d0572d95ba5e89753cbbf94b29ebf8464482108
Muh. Fajri Farid <fajrijry14@gmail.com>
2024-03-22 01:02:36 +0800

    hamburger: merapikan hamburger pada responsifnya

--- script.js
+++ script.js
@@ -1,35 +1,18 @@
 document.addEventListener("DOMContentLoaded", function () {
   const hamburgerBtn = document.getElementById("hamburger-btn");
-  const closeMenuBtn = document.getElementById("close-menu-btn");
   const mobileMenu = document.getElementById("mobile-menu");
-  const overlay = document.getElementById("overlay");
+  const overlay = document.createElement("div");
+  overlay.classList.add("overlay");
+  document.body.appendChild(overlay);
 
   hamburgerBtn.addEventListener("click", function () {
-    mobileMenu.classList.add("show");
-    overlay.style.display = "block"; // Tampilkan overlay saat menu dibuka
-  });
-
-  closeMenuBtn.addEventListener("click", function () {
-    mobileMenu.classList.remove("show");
-    overlay.style.display = "none"; // Sembunyikan overlay saat menu ditutup
+    mobileMenu.classList.toggle("show"); // Toggle class show pada menu
+    overlay.classList.toggle("show"); // Toggle class show pada overlay
   });
 
   // Menutup menu saat overlay diklik
-  overlay.addEventListener("click", function (event) {
-    // Periksa apakah yang diklik adalah overlay dan bukan bagian dari menu
-    if (event.target === overlay) {
-      mobileMenu.classList.remove("show");
-      overlay.style.display = "none"; // Sembunyikan overlay saat overlay diklik
-    }
-  });
-
-  // Menutup menu saat konten di luar menu diklik
-  document.addEventListener("click", function (event) {
-    const isClickInsideMenu = mobileMenu.contains(event.target);
-    const isClickInsideHamburger = hamburgerBtn.contains(event.target);
-    if (!isClickInsideMenu && !isClickInsideHamburger) {
-      mobileMenu.classList.remove("show");
-      overlay.style.display = "none"; // Sembunyikan overlay saat konten di luar menu diklik
-    }
+  overlay.addEventListener("click", function () {
+    mobileMenu.classList.remove("show"); // Hilangkan class show pada menu
+    overlay.classList.remove("show"); // Hilangkan class show pada overlay
   });
 });
