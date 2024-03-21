19e1708e5a3d2d18b43694851c531a6f15d0b597
Muh. Fajri Farid <fajrijry14@gmail.com>
2024-03-22 00:26:17 +0800

    test

--- script.js
+++ script.js
@@ -2,12 +2,34 @@ document.addEventListener("DOMContentLoaded", function () {
   const hamburgerBtn = document.getElementById("hamburger-btn");
   const closeMenuBtn = document.getElementById("close-menu-btn");
   const mobileMenu = document.getElementById("mobile-menu");
+  const overlay = document.getElementById("overlay");
 
   hamburgerBtn.addEventListener("click", function () {
     mobileMenu.classList.add("show");
+    overlay.style.display = "block"; // Tampilkan overlay saat menu dibuka
   });
 
   closeMenuBtn.addEventListener("click", function () {
     mobileMenu.classList.remove("show");
+    overlay.style.display = "none"; // Sembunyikan overlay saat menu ditutup
+  });
+
+  // Menutup menu saat overlay diklik
+  overlay.addEventListener("click", function (event) {
+    // Periksa apakah yang diklik adalah overlay dan bukan bagian dari menu
+    if (event.target === overlay) {
+      mobileMenu.classList.remove("show");
+      overlay.style.display = "none"; // Sembunyikan overlay saat overlay diklik
+    }
+  });
+
+  // Menutup menu saat konten di luar menu diklik
+  document.addEventListener("click", function (event) {
+    const isClickInsideMenu = mobileMenu.contains(event.target);
+    const isClickInsideHamburger = hamburgerBtn.contains(event.target);
+    if (!isClickInsideMenu && !isClickInsideHamburger) {
+      mobileMenu.classList.remove("show");
+      overlay.style.display = "none"; // Sembunyikan overlay saat konten di luar menu diklik
+    }
   });
 });
